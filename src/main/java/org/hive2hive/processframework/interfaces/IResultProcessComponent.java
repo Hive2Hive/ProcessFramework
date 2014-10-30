47c4b7d0a10fc27a96160ef07f97745867f5d434
Christian Lüthold <biocodr@gmail.com>
2014-10-30 15:27:20 +0100

    improved IResultProcessComponent: docu
    Signed-off-by: Christian Lüthold <biocodr@gmail.com>

--- src/main/java/org/hive2hive/processframework/interfaces/IResultProcessComponent.java
+++ src/main/java/org/hive2hive/processframework/interfaces/IResultProcessComponent.java
@@ -1,7 +1,7 @@
 package org.hive2hive.processframework.interfaces;
 
 /**
- * Interface for process components that intend to deliver a result.
+ * Interface for process components that deliver a result.
  * 
  * @author Christian Lüthold
  * 
@@ -9,25 +9,28 @@ package org.hive2hive.processframework.interfaces;
  */
 public interface IResultProcessComponent<T> extends IProcessComponent {
 
+	// TODO controversy about whether this should be public, ProcessComponent notifications are private and
+	// notification happens through the attached listeners
+	void notifyResultComputed(T result);
+
 	/**
-	 * Returns the computed result. Maybe <code>null</code> in case the computation has not finished yet.
-	 * @return Result or <code>null</code> if computation not finished.
+	 * Gets the result computed by this {@code IResultProcessComponent}.
+	 * 
+	 * @return The computed result or <code>null</code> if the computation has not yet finished.
 	 */
 	T getResult();
-	
-	void notifyResultComputed(T result);
-	
+
 	/**
-	 * Attaches an {@link IProcessResultListener} to the process component.
+	 * Attaches an {@link IProcessResultListener} to this {@code IResultProcessComponent}.
 	 * 
-	 * @param listener The listener to be attached.
+	 * @param listener The {@link IProcessResultListener} to be attached.
 	 */
 	void attachListener(IProcessResultListener<T> listener);
 
 	/**
-	 * Detaches an {@link IProcessResultListener} from the process component.
+	 * Detaches an {@link IProcessResultListener} to this {@code IResultProcessComponent}.
 	 * 
-	 * @param listener The listener to be detached.
+	 * @param listener The {@link IProcessResultListener} to be detached.
 	 */
 	void detachListener(IProcessResultListener<T> listener);
 
