fa11fdc6d7f8624f6a5ec15740e8b69c0aa3f3ba
Christian Lüthold <biocodr@gmail.com>
2014-10-31 12:22:11 +0100

    ProcessState.FAILED transitions tested
    Signed-off-by: Christian Lüthold <biocodr@gmail.com>

--- src/test/java/org/hive2hive/processframework/ProcessStateTest.java
+++ src/test/java/org/hive2hive/processframework/ProcessStateTest.java
@@ -215,6 +215,42 @@ public class ProcessStateTest extends BaseTest {
 		}
 	}
 	
+	@Test
+	public void testFailedState() {
+
+		IProcessComponent comp = TestUtil.sampleComponent();
+
+		// use reflection to set internal process state
+		setState(comp, ProcessState.FAILED);
+		assertTrue(comp.getState() == ProcessState.FAILED);
+
+		// test invalid operations
+		try {
+			comp.start();
+			fail("InvalidProcessStateException should have been thrown.");
+		} catch (InvalidProcessStateException ex) {
+			// should happen
+		}
+		try {
+			comp.pause();
+			fail("InvalidProcessStateException should have been thrown.");
+		} catch (InvalidProcessStateException ex) {
+			// should happen
+		}
+		try {
+			comp.resume();
+			fail("InvalidProcessStateException should have been thrown.");
+		} catch (InvalidProcessStateException ex) {
+			// should happen
+		}
+		try {
+			comp.cancel(TestUtil.sampleRollbackReason());
+			fail("InvalidProcessStateException should have been thrown.");
+		} catch (InvalidProcessStateException ex) {
+			// should happen
+		}
+	}
+	
 	/**
 	 * Uses reflection to set the internal state of a process component.
 	 * 
