455bed89e053a6a8acfe82c794e35fe816c71e13
Christian Lüthold <biocodr@gmail.com>
2014-10-31 13:36:19 +0100

    introduce ProcessRollbackException
    Signed-off-by: Christian Lüthold <biocodr@gmail.com>

---
+++ src/main/java/org/hive2hive/processframework/exceptions/ProcessRollbackException.java
@@ -0,0 +1,15 @@
+package org.hive2hive.processframework.exceptions;
+
+/**
+ * Exception that occurs due to a failure during a process component's rollback.
+ * 
+ * @author Christian Lüthold
+ * 
+ */
+public class ProcessRollbackException extends Exception {
+
+	private static final long serialVersionUID = -5082116677429110685L;
+
+	// TODO make this exception class similar to the ProcessExecutionException, see
+	// https://github.com/Hive2Hive/ProcessFramework/issues/9
+}
