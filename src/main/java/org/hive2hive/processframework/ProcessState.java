db9b41462e4704fdfd4fca1c179215d49d897853
Christian Lüthold <biocodr@gmail.com>
2014-11-15 20:41:00 +0100

    typos
    Signed-off-by: Christian Lüthold <biocodr@gmail.com>

--- src/main/java/org/hive2hive/processframework/ProcessState.java
+++ src/main/java/org/hive2hive/processframework/ProcessState.java
@@ -11,36 +11,36 @@ import org.hive2hive.processframework.interfaces.IProcessComponent;
 public enum ProcessState {
 
 	/**
-	 * Represents a {@link IProcessComponent} that is ready to be executed.
+	 * Represents an {@link IProcessComponent} that is ready to be executed.
 	 */
 	READY,
 	/**
-	 * Represents a {@link IProcessComponent} that is currently being executed.
+	 * Represents an {@link IProcessComponent} that is currently being executed.
 	 */
 	EXECUTING,
 	/**
-	 * Represents a {@link IProcessComponent} that has executed successfully.
+	 * Represents an {@link IProcessComponent} that has executed successfully.
 	 */
 	EXECUTION_SUCCEEDED,
 	/**
-	 * Represents a {@link IProcessComponent} that has executed unsuccessfully due to cancellation or a
+	 * Represents an {@link IProcessComponent} that has executed unsuccessfully due to cancellation or a
 	 * failure.
 	 */
 	EXECUTION_FAILED,
 	/**
-	 * Represents a {@link IProcessComponent} that is currently being rolled back.
+	 * Represents an {@link IProcessComponent} that is currently being rolled back.
 	 */
 	ROLLBACKING,
 	/**
-	 * Represents a {@link IProcessComponent} that has rolled back successfully.
+	 * Represents an {@link IProcessComponent} that has rolled back successfully.
 	 */
 	ROLLBACK_SUCCEEDED,
 	/**
-	 * Represents a {@link IProcessComponent} that has rolled back unsuccessfully due to a failure.
+	 * Represents an {@link IProcessComponent} that has rolled back unsuccessfully due to a failure.
 	 */
 	ROLLBACK_FAILED,
 	/**
-	 * Represents a {@link IProcessComponent} that is currently paused, whether it is executing or rolling
+	 * Represents an {@link IProcessComponent} that is currently paused, whether it is executing or rolling
 	 * back.
 	 */
 	PAUSED
